b6fe425e987a08d58ed3a5775a538375891984a8
lukailun <kailun@celer.network>
2021-07-22 22:35:57 +0800

    script init

---
+++ clean-xcode.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Removing Derived Data..."
+rm -rf ~/Library/Developer/Xcode/DerivedData/
+
+echo "Removing Device Support..."
+rm -rf ~/Library/Developer/Xcode/iOS\ DeviceSupport
+rm -rf ~/Library/Developer/Xcode/watchOS\ DeviceSupport
+rm -rf ~/Library/Developer/Xcode/tvOS\ DeviceSupport
+
+echo "Removing Unavailable Simulators..."
+xcrun simctl delete unavailable
+
+echo "Removing Caches..."
+rm -rf ~/Library/Caches/com.apple.dt.Xcode
+rm -rf ~/Library/Caches/org.carthage.CarthageKit
+
+if command -v pod  &> /dev/null
+then
+    pod cache clean --all
+fi
+
+echo "Done!"
